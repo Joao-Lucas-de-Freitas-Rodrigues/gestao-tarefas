1f383d1d5b691a38d35de8b8ff29c967b09ddec0
Lucas Freitas <joaolucasfreitas360@gmail.com>
2025-08-10 23:03:15 -0300

    feat(Enums): Enums criados

---
+++ GestaoTarefas/Models/Enums/TodoPriority.cs
@@ -0,0 +1,10 @@
+﻿namespace GestaoTarefas.Models.Enums
+{
+    public enum TodoPriority
+    {
+        Baixa = 0,
+        Normal = 1,
+        Alta = 2,
+        Crítica = 3
+    }
+}
---
+++ GestaoTarefas/Models/Enums/TodoStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace GestaoTarefas.Models.Enums
+{
+    public enum TodoStatus
+    {
+        Aberto = 0,
+        Andamento = 1,
+        Feito = 2,
+        Arquivado = 3
+    }
+}
